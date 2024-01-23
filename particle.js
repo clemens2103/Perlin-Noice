cfc72da2e146c361ee2976ea90efa89ef21ff110
Clemens <clemens_schlosser@web.de>
2024-01-23 22:06:31 +0100

    Resizing Buffer

--- particle.js
+++ particle.js
@@ -1,61 +1,60 @@
 class Particle {
-    constructor() {
-      this.pos = createVector(random(width), random(height));
-      this.vel = createVector(0, 0);
-      this.acc = createVector(0, 0);
-      this.maxspeed = 4;
-      this.prevPos = this.pos.copy();
-    }
-  
-    update() {
-      this.vel.add(this.acc);
-      this.vel.limit(this.maxspeed);
-      this.pos.add(this.vel);
-      this.acc.mult(0);
-    }
-  
-    follow(vectors) {
-      var x = floor(this.pos.x / scl);
-      var y = floor(this.pos.y / scl);
-      var index = x + y * cols;
-      var force = vectors[index];
-      this.applyForce(force);
-    }
-  
-    applyForce(force) {
-      this.acc.add(force);
-    }
-  
-    show() {
-      stroke(255, 10);
-      strokeWeight(1);
-      line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+  constructor() {
+    this.pos = createVector(random(width), random(height)); // Initial position set to a random location
+    this.vel = createVector(0, 0); // Initial velocity set to zero
+    this.acc = createVector(0, 0); // Initial acceleration set to zero
+    this.maxspeed = 4; // Maximum speed of the particle
+    this.prevPos = this.pos.copy(); // Storing the previous position for drawing
+  }
+
+  update() {
+    this.vel.add(this.acc); // Add acceleration to velocity
+    this.vel.limit(this.maxspeed); // Limit the velocity to the maximum speed
+    this.pos.add(this.vel); // Update position based on velocity
+    this.acc.mult(0); // Reset acceleration
+  }
+
+  follow(vectors) {
+    var x = floor(this.pos.x / scl); // Calculate x index in the flowfield
+    var y = floor(this.pos.y / scl); // Calculate y index in the flowfield
+    var index = x + y * cols; // Calculate overall index in the flowfield
+    var force = vectors[index]; // Get the force vector from the flowfield
+    this.applyForce(force); // Apply this force
+  }
+
+  applyForce(force) {
+    this.acc.add(force); // Add force to acceleration
+  }
+
+  show(pg) {
+    pg.stroke(149, 184, 191); // Set stroke color for drawing
+    pg.strokeWeight(1); // Set stroke weight
+    pg.line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y); // Draw a line from the previous position to the current position
+    this.updatePrev(); // Update the previous position
+  }
+
+  updatePrev() {
+    this.prevPos.x = this.pos.x; // Set previous x position
+    this.prevPos.y = this.pos.y; // Set previous y position
+  }
+
+  edges() {
+    // Check if the particle is out of the canvas bounds and wrap it around
+    if (this.pos.x > width) {
+      this.pos.x = 0;
       this.updatePrev();
     }
-  
-    updatePrev() {
-      this.prevPos.x = this.pos.x;
-      this.prevPos.y = this.pos.y;
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
     }
-  
-    edges() {
-      if (this.pos.x > width) {
-        this.pos.x = 0;
-        this.updatePrev();
-      }
-      if (this.pos.x < 0) {
-        this.pos.x = width;
-        this.updatePrev();
-      }
-      if (this.pos.y > height) {
-        this.pos.y = 0;
-        this.updatePrev();
-      }
-      if (this.pos.y < 0) {
-        this.pos.y = height;
-        this.updatePrev();
-      }
-  
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
     }
-  
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
   }
+}
--- sketch.js
+++ sketch.js
@@ -1,30 +1,32 @@
 var inc = 0.1;
-var scl = 10;
+var scl = 15;
 var cols, rows;
 var zoff = 0;
-var fr;
 var particles = [];
 var flowfield;
-
-
+var buffer; // Buffer for off-screen drawing
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cols = floor(width / scl);
-  rows = floor(height / scl);
-  fr = createP('');
+  cols = floor(width / scl); // Calculate number of columns based on scale
+  rows = floor(height / scl); // Calculate number of rows based on scale
 
-  flowfield = new Array(cols * rows);
+  flowfield = new Array(cols * rows); // Initialize flowfield array
 
-  for (var i = 0; i < 300; i++) {
-    particles[i] = new Particle();
+  for (var i = 0; i < 150; i++) {
+    particles[i] = new Particle(); // Create particles
   }
-  background(51);
+  buffer = createGraphics(windowWidth, windowHeight); // Create off-screen graphics buffer
+  buffer.background('#1e2526'); // Set background color of buffer
 }
 
-
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight); // Resize canvas when window is resized
+  buffer.resizeCanvas(windowWidth, windowHeight); // Resize buffer accordingly
+  buffer.background('#1e2526'); // Reset background color after resizing
+  cols = floor(width / scl); // Recalculate columns after resize
+  rows = floor(height / scl); // Recalculate rows after resize
+  flowfield = new Array(cols * rows); // Reinitialize flowfield array
 }
 
 function draw() {
@@ -32,32 +34,23 @@ function draw() {
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
-      var index = x + y * cols;
-      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
-      var v = p5.Vector.fromAngle(angle);
-      v.setMag(1);
-      flowfield[index] = v;
+      var index = x + y * cols; // Calculate index in flowfield array
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4; // Use Perlin noise to determine angle
+      var v = p5.Vector.fromAngle(angle); // Create vector from angle
+      v.setMag(1); // Set magnitude of vector
+      flowfield[index] = v; // Store vector in flowfield array
       xoff += inc;
-      stroke(0, 50);
-      // push();
-      // translate(x * scl, y * scl);
-      // rotate(v.heading());
-      // strokeWeight(1);
-      // line(0, 0, scl, 0);
-      // pop();
     }
     yoff += inc;
-
-    zoff += 0.0003;
+    zoff += 0.0003; // Increment z-offset for noise
   }
 
   for (var i = 0; i < particles.length; i++) {
-    particles[i].follow(flowfield);
-    particles[i].update();
-    particles[i].edges();
-    particles[i].show();
-    window.addEventListener('resize', windowResized);
+    particles[i].follow(flowfield); // Particles follow the flowfield
+    particles[i].update(); // Update particle position
+    particles[i].edges(); // Handle particles reaching edges
+    particles[i].show(buffer); // Draw particles on the buffer
   }
 
-  // fr.html(floor(frameRate()));
+  image(buffer, 0, 0); // Draw the buffer on the main canvas
 }
